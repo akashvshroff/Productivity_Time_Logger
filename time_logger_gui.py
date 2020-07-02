4a3f4ed8b6a68c88278d09adfabe10aacf1450e9
Akash Vikram Shroff <63399889+akashvshroff@users.noreply.github.com>
2020-07-02 13:44:01 +0530

    Going to work on a new branch

--- time_logger_gui.py
+++ time_logger_gui.py
@@ -5,7 +5,7 @@ from openpyxl import load_workbook
 from openpyxl.styles import Border, Side, Alignment, colors, PatternFill, Font, Fill
 from openpyxl.styles.colors import Color
 from openpyxl.utils import get_column_letter
-from datetime import date, timedelta
+from datetime import date, timedelta, datetime
 import os
 import sys
 import shelve
